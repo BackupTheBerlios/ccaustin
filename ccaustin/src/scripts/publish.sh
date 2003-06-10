52f283ec126b6934609d20df01c531e949732cd6
javajames27 <javajames27>
2003-06-10 16:35:11 +0000

    Fixed link error

--- ccaustin/src/scripts/publish.sh
+++ ccaustin/src/scripts/publish.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
 #
-# Performs a publish of the latest code from the ccaustin module,
-# executes the build, and ftps the results to the production server
+# Performs a checkout/update of the latest code from the ccaustin module,
+# executes the build, and copies the results to /daily under the html root
 #
 echo Starting publish at `date`
 export CVSROOT=javajames27@cvs.ccaustin.berlios.de:/cvsroot/ccaustin
 export CVS_RSH=ssh
-export PROJECT_ROOT=/home/users/javajames27
-export DAILY_DIR=$PROJECT_ROOT/.daily
-export DEPLOY_DIR=/home/groups/ccaustin/htdocs/daily
+export PROJECT_ROOT=/home/james
+export WORK_DIR=$PROJECT_ROOT/.websandbox
+export DEPLOY_DIR=/usr/local/webapps/apache/htdocs
 
-export CALVARY_HOME=$DAILY_DIR/ccaustin
-export ANT_HOME=$DAILY_DIR/ccaustin/thirdparty/ant
-export JAVA_HOME=$PROJECT_ROOT/jdk1.3.1_02
+export CALVARY_HOME=$WORK_DIR/ccaustin
+export ANT_HOME=$WORK_DIR/ccaustin/thirdparty/ant
+export JAVA_HOME=/usr/local/jdk
 PATH=$CALVARY_HOME/bin:$ANT_HOME/bin:$JAVA_HOME/bin:$PATH
+PATH=$CALVARY_HOME/bin:$PATH
 
 export ANT_OPTS="-Ddeploy.dir=$DEPLOY_DIR"
 
@@ -25,19 +26,19 @@ echo JAVA_HOME=$JAVA_HOME
 # If not, perform a checkout
 # Otherwise, do an update
 
-if [ ! -d $DAILY_DIR ]; then
-	echo First Time: Checking out daily...
-	mkdir $DAILY_DIR
-	cd $DAILY_DIR
-	cvs co ccaustin
-	cd ccaustin
+if [ ! -d $WORK_DIR ]; then
+        echo First Time: Creating a new sandbox...
+        mkdir $WORK_DIR
+        cd $WORK_DIR
+        cvs co ccaustin
+        cd ccaustin
 else
-	echo Updating daily...
-	cd $DAILY_DIR/ccaustin
-	cvs update -Pd
+        echo Updating sandbox...
+        cd $WORK_DIR/ccaustin
+        cvs update -Pd
 fi
 
 # Do the work
-ant web publish
+ant web deploy
 
 echo Ending publish at `date`
