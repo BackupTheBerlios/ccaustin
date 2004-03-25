7e1d091f7aeb0d3baa06a744b342447687b1e444
gregk <gregk>
2004-03-25 12:57:08 +0000

    *** empty log message ***

---
+++ ccaustin/src/mom-web/js/events/mo_menu.js
@@ -0,0 +1,39 @@
+
+<!-- Begin
+
+
+document.write('<DIV id=mo_menu>');
+document.write('<TABLE cellpadding=0 cellspacing=0 border=0><tr><td>');
+document.write('<img src="picts/menu-top.gif"></a><br>');
+
+document.write('</td></tr><tr><form action="index.html"><td>');
+document.write('<input type="submit" value="Home" onmouseover="this.className=\'buttonon\'" onmouseout="this.className=\'button\'" class="button"><br>');
+
+document.write('</td></tr><tr></form><form action="about.htm"><td>');
+document.write('<input type="submit" value="Calvary Austin" onmouseover="this.className=\'buttonon\'" onmouseout="this.className=\'button\'" class="button"><br>');
+
+document.write('</td></tr><tr></form><form action="notes.htm"><td>');
+document.write('<input type="submit" value="Study Guide" onmouseover="this.className=\'buttonon\'" onmouseout="this.className=\'button\'" class="button"><br>');
+
+document.write('</td></tr><tr></form><form action="messinfo.htm"><td>');
+document.write('<input type="submit" value="Message Info" onmouseover="this.className=\'buttonon\'" onmouseout="this.className=\'button\'" class="button"><br>');
+
+document.write('</td></tr><tr></form><form action="givingmp3.htm"><td>');
+document.write('<input type="submit" value="Giving Message" onmouseover="this.className=\'buttonon\'" onmouseout="this.className=\'button\'" class="button"><br>');
+
+document.write('</td></tr><tr></form><form action="bmmp3.htm"><td>');
+document.write('<input type="submit" value="Vision Message" onmouseover="this.className=\'buttonon\'" onmouseout="this.className=\'button\'" class="button"><br>');
+
+document.write('</td></tr><tr></form><form action="contact.htm"><td>');
+document.write('<input type="submit" value="Contact" onmouseover="this.className=\'buttonon\'" onmouseout="this.className=\'button\'" class="button"><br>');
+
+
+
+document.write('<img src="picts/menu-center.gif" width="125" height="88"></a><br>');
+document.write('</td></tr><tr><td bgcolor="#000000" background="picts/menu-bottom.gif" height="250" valign="top">');
+document.write('<TABLE cellpadding=30 cellspacing=0 border=0><tr><td>');
+document.write('<span class="whitecopy">Welcome to the Building Momentum message CD website!</span><BR>');
+document.write('</td></tr></table>');
+document.write('</td></tr></table></DIV>');
+
+//  End -->
